fe2163b8f3820d8d4a5aeef765231ae1c9c0de16
toyia0 <toyia0@gmail.com>
2025-12-09 17:15:31 +0000

    Started Portfolio

---
+++ scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
